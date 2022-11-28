970c115449527dbe63283ac3e9705363b09ab556
Hammad Amin <hammad.amin@norgic.com>
2022-11-29 01:56:23 +0500

    published on npm and tested on new demo

--- example/index.tsx
+++ example/index.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { AppRegistry } from 'react-native';
 import App from './src/App';
 import notifee  from '@notifee/react-native';
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import * as React from 'react';
 
 import { StyleSheet, View, Text } from 'react-native';
